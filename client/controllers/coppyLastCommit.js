3cc5ac733b69d53aa14d80f2068045658a052ffe
jasja1231 <jasja1231@gmail.com>
2015-06-12 01:25:33 +0200

    fdsfd

--- client/controllers/coppyLastCommit.js
+++ client/controllers/coppyLastCommit.js
@@ -5,24 +5,22 @@ app.controller('mainCtrl', function($http , $scope ,$timeout ){
     $scope.messages =["first","second"];
     $scope.pictures =["first","second"];
 
-   var action = new Presenter();
+   var action = new Consumer();
    var act = function() {
-        $timeout(action.presentation(), 1000);
+        $timeout(action.poll(), 1000);
     };     
      act();
-   
     function Consumer() {
-      this.point;/*why undefined????*/
+      this.point =0;
       var regexpBase64 = /^\s*(?:(?:[A-Za-z0-9+]{4})+\s*)*[A-Za-z0-9+]*={0,2}\s*$/;
-      this.poll = function(data){
+      return{
+      poll : function(data){
        $http.post('http://localhost:3000/api/form', data)
       .success(function (res ) {
         if ((res.length%4==0)&&(res.search(regexpBase64)!==-1)&&(res.length!==0)){
-          this.point=0;
               $scope.pictures.push(res);
               console.log("pic:"+res);
           }else {
-            this.point=1;
               $scope.messages.push(res);
               console.log("message:"+res);
           }
@@ -32,26 +30,23 @@ app.controller('mainCtrl', function($http , $scope ,$timeout ){
               });
              /* angular.element('.name').addClass('afterSend');*/
    }
- }
-  
+  };
+  }
+
   function Presenter() {
-         return {
-          presentation : function(){
+         this.presentation = function(){
             var consumer = new Consumer();
             consumer.poll();
-            alert(consumer.point);/*why undefined??*/
             if (consumer.point == 1){
                return angular.element('#current_message').append('consumer.currentData.data');
              }else{
               var arg=angular.element();
-              return /*angular.element('#current_message').append('<img src="data:image/png;base64,{{consumer.currentData.data}}" alt="base64 picture">');*/
-             }
-         },
-         lifetime : function(){
+              return angular.element('#current_message').append('<img src="data:image/png;base64,{{consumer.currentData.data}}" alt="base64 picture">');
+             };
+         this.lifetime = function(){
              return false;
          }
-       }
     }
-    var pr = new Presenter();
-   pr.presentation();
+  };
+    
 });
