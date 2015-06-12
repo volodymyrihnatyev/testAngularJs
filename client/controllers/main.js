253b6208ef42f380156d3cea0ba6339a91483b87
jasja1231 <jasja1231@gmail.com>
2015-06-12 11:52:49 +0200

    almost done

--- client/controllers/main.js
+++ client/controllers/main.js
@@ -1,34 +1,38 @@
 var app = angular.module('testApp');
 
 
-app.controller('mainCtrl', function($http , $scope ,$timeout ){
+app.controller('mainCtrl', function($http , $scope ,$interval ){
     $scope.messages =["first","second"];
-    $scope.pictures =["first","second"];
+    $scope.pictures =["TWFyeSBoYWQgYSBsaXR0bGUgbGFtYi4uLiBASktMTU5PUFpbXF1eX2BhamtsbW5ven1+","second"];
 
-   var action = new Presenter();
+
+   var action = new Presenter();//Create Presenter object
    var act = function() {
-        $timeout(action.presentation(), 1000);
+        $interval(action.presentation(), 1000);//poll data from server every 1second
+        action.remove(1000);//remove data from DOM element with id 'current_message' every 1 second
     };     
-     act();
-   
+
+     act();//run function act()
 
     function Consumer() {
-      this.point =undefined;
-      this.currentData = {};
-      regexpBase64 : /^\s*(?:(?:[A-Za-z0-9+]{4})+\s*)*[A-Za-z0-9+]*={0,2}\s*$/;
+      this.point;
+      this.currentData ;
+      regexpBase64 = /^\s*(?:(?:[A-Za-z0-9+]{4})+\s*)*[A-Za-z0-9+]*={0,2}\s*$/;
       this.poll = function(data){
        $http.post('http://localhost:3000/api/form', data)
       .success(function (res ) {
-        if ((res.length%4==0)&&(res.search(regexpBase64)!==-1)&&(res.length!==0)){
-             this.point=1;
+        if ((res.length%4==0)&&(res.search(regexpBase64)!==-1)&&(res.length!==0)){//checking message for being Base64 encoded
               $scope.pictures.push(res);
               console.log("pic:"+res);
-              this.currentData.data = res;
+              this.currentData= res;
+              this.point=0;
+              console.log("pic:"+this.point+this.currentData);
           }else {
-              this.point=1;
               $scope.messages.push(res);
               console.log("message:"+res);
-              this.currentData.data = res;
+              this.currentData = res;
+              this.point=1;
+              console.log("pic:"+this.point+this.currentData);
           }
         })
       .error(function(error) {
@@ -39,23 +43,22 @@ app.controller('mainCtrl', function($http , $scope ,$timeout ){
  }
   
   function Presenter() {
-         return {
-          presentation : function(){
-            var consumer = new Consumer();
+          consumer = new Consumer();
+          this.presentation = function(){
             consumer.poll();
             alert(consumer.point);/*why undefined??*/
             if (consumer.point == 1){
               var myEl = angular.element( document.querySelector( '#current_message' ) );
-               return  myEl.prepend('{{consumer.currentData.data}}');
+               return  myEl.prepend('{{consumer.currentData}}');
              }else{
               var myEl = angular.element( document.querySelector( '#current_message' ) );
-              return myEl.prepend('<img src="data:image/png;base64,{{consumer.currentData.data}}" alt="base64 picture">');
+              return myEl.prepend('<img src="data:image/png;base64,{{consumer.currentData}}" alt="base64 picture">');
              }
-         },
-         lifetime : function(){
-             return false;
-         }
-       }
+         };
+          this.remove = function(time){
+            var myEl = angular.element( document.querySelector( '#current_message' ) );
+            $interval(function(){myEl.children().remove()},time);
+         };
     }
 });
 
